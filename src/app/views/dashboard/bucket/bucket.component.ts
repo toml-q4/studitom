723f9ac019b328c3e0e83d77ed5e6cdeb08a9d16
Tom Luong <toml@q4inc.com>
2018-02-12 15:01:54 -0500

    update (#2)

--- src/app/views/dashboard/bucket/bucket.component.ts
+++ src/app/views/dashboard/bucket/bucket.component.ts
@@ -2,22 +2,23 @@ import { Component, OnInit, Input } from '@angular/core';
 import { BucketModel } from 'app/views/dashboard/bucket/bucket.model';
 import { Bucket } from './bucket';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 import { Workflow } from '../workflow/workflow';
 import { WorkflowService } from '../shared/workflow.service';
 
 @Component({
   selector: 'q4-bucket',
   templateUrl: './bucket.component.html',
-  styleUrls: ['./bucket.component.css']
+  styleUrls: ['./bucket.component.scss']
 })
 export class BucketComponent implements OnInit {
   @Input() bucket: Bucket;
-  workflows$: Observable<Workflow[]>;
+  workflowIds$: Observable<string[]>;
   constructor(private workflowService: WorkflowService) {
   }
 
   ngOnInit() {
-    this.workflows$ = this.workflowService.getByBucketId(this.bucket.id);
+    this.workflowIds$ = this.workflowService.getByBucketId(this.bucket.id).map(workflows => workflows.map(function(w){ return w.id; }));
   }
 
   trackWorkflow(index, workflow) {
