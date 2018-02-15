23bc5812546bb24f53a5fc621fd814a09682c27f
toml@q4inc.com <tomluong@live.com>
2018-02-15 00:26:46 -0500

    update

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
+  responsive = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  show() {
+    this.responsive = !this.responsive;
   }
 
 }
