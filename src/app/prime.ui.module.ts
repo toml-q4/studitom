b04dfc9f149df594673b2439211104ddc8c07e40
toml@q4inc.com <tomluong@live.com>
2018-02-16 16:36:17 -0500

    update

--- src/app/prime.ui.module.ts
+++ src/app/prime.ui.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { MenuModule } from 'primeng/menu';
-import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+
 @NgModule({
     imports:
         [
           TableModule,
           MenuModule,
-          ButtonModule
+          ButtonModule,
+          DropdownModule
         ],
     exports:
         [
           TableModule,
           MenuModule,
-          ButtonModule
+          ButtonModule,
+          DropdownModule
         ]
 })
 export class PrimeUiModule { }
--- src/app/views/buckets/buckets.component.ts
+++ src/app/views/buckets/buckets.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Bucket } from '../shared/domains/bucket';
 import { BucketService } from '../shared/services/bucket.service';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'q4-buckets',
@@ -11,7 +12,13 @@ import { BucketService } from '../shared/services/bucket.service';
 export class BucketsComponent implements OnInit {
 
   buckets$: Observable<Bucket[]>;
-  constructor(private bucketService: BucketService) { }
+  bucketSelections: SelectItem[];
+  bucketSelection: Bucket;
+  constructor(private bucketService: BucketService) {
+    this.bucketSelections = [
+      { label: 'Everything active', value: { id: 1, name: 'filter name' } }
+    ];
+  }
 
   ngOnInit() {
     this.buckets$ = this.bucketService.get();
